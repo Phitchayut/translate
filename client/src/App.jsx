d091b8dcedeae32ee409530c34f018691520e9d3
Phitchayut <beam.phitchayut@gmail.com>
2023-08-09 00:00:16 +0700

    finish

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Story from "./components/Story";
 
-const text = "A massive and notoriously mischievous black bear has been captured after more than a year on the run following a string of break-ins in California. The 500lbs (227kg) bear, known to fans as Hank the Tank, is believed to have broken into 21 homes in the Lake Tahoe area since 2022.Hank - who is actually a female bear registered as 64F - will be moved to a sanctuary in Colorado. Her three cubs may be transferred to a facility in California's Sonoma County. In a statement released on Friday, the California Department of Fish and Wildlife (CDFW) said that biologists had safely immobilised the bear, who had been linked to 21 break-ins by DNA evidence."
+const text = "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to Naviglio where you can enjoy the main night life in Barcelona."
 
 function App() {
   return (
--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -1,43 +1,71 @@
 import React, { useState } from "react";
 import { tran } from "../fuctions/tran";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+  Popover,
+  PopoverHandler,
+  PopoverContent,
+  Spinner,
+} from "@material-tailwind/react";
 
 const Story = ({ text }) => {
   const [wordTran, setWordTran] = useState(null);
+  const [clickedWord, setClickedWord] = useState(null);
   const words = text.split(" ");
   const handleClick = (word) => {
-    console.log(word);
     tran({ word })
       .then((res) => {
         setWordTran(res.data[0].translations[0].text);
       })
       .catch((err) => console.log(err));
+    setClickedWord(word);
+    setWordTran(null)
   };
-  alert(wordTran)
+  // console.log(wordTran);
+
   return (
     <div className="container mx-auto py-5">
       <div className="flex justify-center">
-        <div className="card w-[32rem] bg-base-100 shadow-xl">
-          <figure>
+        <Card className="mt-6 w-96">
+          <CardHeader color="blue-gray" className="relative h-56">
             <img
-              src="https://ichef.bbci.co.uk/news/976/cpsprodpb/14A4B/production/_123355548_273225536_5387239747969927_3174953441515970260_n.jpg.webp"
-              alt="Shoes"
-              className="w-full h-[20rem] object-cover"
+              src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+              alt="card-image"
             />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Bear!!</h2>
-            <div>
-              {words.map((word, index) => (
+          </CardHeader>
+          <CardBody>
+            <Typography variant="h5" color="blue-gray" className="mb-2">
+              UI/UX Review Check
+            </Typography>
+            {words.map((word, index) => {
+              return (
                 <React.Fragment key={index}>
-                  <span onClick={() => handleClick(word, index)}>
-                    {word}
-                    </span>
-                    {" "}
+                  {word === clickedWord ? (
+                    <>
+                      <Popover>
+                        <PopoverHandler>
+                          <span onClick={() => handleClick(word)}>{word} </span>
+                        </PopoverHandler>
+                        <PopoverContent>
+                            {wordTran == null ? (<Spinner />) : (<p>{wordTran}</p>)}
+                        </PopoverContent>
+                      </Popover>
+                    </>
+                  ) : (
+                    <>
+                    <span onClick={() => handleClick(word)}>{word} </span>
+                    </>
+                  )}
                 </React.Fragment>
-              ))}
-            </div>
-          </div>
-        </div>
+              );
+            })}
+          </CardBody>
+        </Card>
       </div>
     </div>
   );
